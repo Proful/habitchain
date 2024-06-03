a6034c01dc7f449e4f093a183e91419c33b05c23
Proful <proful.sadangi@gmail.com>
2024-06-03 20:13:26 +0530

    swipe

--- ui/app/routes/habits.$habitName.tsx
+++ ui/app/routes/habits.$habitName.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from '@remix-run/node'
 import { useParams } from '@remix-run/react'
 import { useEffect, useState } from 'react'
+import { useSwipeable } from 'react-swipeable'
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,54 +10,64 @@ export const meta: MetaFunction = () => {
   ]
 }
 
-interface HabitData {
-  [key: string]: number[]
+type HabitData = {
+  [habitName: string]: {
+    [year: string]: {
+      [month: string]: number[]
+    }
+  }
 }
-//
-// export async function clientLoader({ params }: ClientLoaderFunctionArgs) {
-//   console.log(params)
-//   const response = localStorage.getItem('habitData')
-//   const data = JSON.parse(response!)
-//   return data
-// }
 
+// Get the current date
+const currentDate = new Date()
+const _currentDay = currentDate.getDate()
+const _currentMonth = currentDate.getMonth() + 1 // getMonth() is zero-based
+const _currentYear = currentDate.getFullYear()
+const _currentMonthName = currentDate.toLocaleString('default', {
+  month: 'short',
+})
 export default function ViewHabit() {
   const { habitName } = useParams()
   const [habitData, setHabitData] = useState<HabitData | null>(null)
   const [clickedDays, setClickedDays] = useState<number[]>([])
   const [isCurrentDayPressed, setIsCurrentDayPressed] = useState<boolean>(false)
   const [loaded, setLoaded] = useState<boolean>(false)
+  const [selectedDay, setSelectedDay] = useState(_currentDay)
+  const [selectedMonth, setSelectedMonth] = useState(_currentMonth)
+  const [selectedYear, setSelectedYear] = useState(_currentYear)
+  const [selectedMonthName, setSelectedMonthName] = useState(_currentMonthName)
+  const [weeks, setWeeks] = useState(null)
 
-  // Get the current date
-  const currentDate = new Date()
-  const currentDay = currentDate.getDate()
-  const currentMonth = currentDate.getMonth() + 1 // getMonth() is zero-based
-  const currentYear = currentDate.getFullYear()
-  const currentMonthName = currentDate.toLocaleString('default', {
-    month: 'short',
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      setSelectedMonth(selectedMonth + 1)
+    },
+    onSwipedRight: () => {
+      setSelectedMonth(selectedMonth - 1)
+    },
   })
 
-  let month = 5,
-    year = 2024
-  // Get the number of days in the month and the day of the week for the first day
-  const daysInMonth = getDaysInMonth(month, year)
-  const firstDayOfWeek = getFirstDayOfWeek(month, year)
-
-  // Generate an array of dates for each day of the month
-  const daysArray = Array.from({ length: daysInMonth }, (_, index) => index + 1)
-
-  // Add empty cells to the beginning to align the start day
-  const alignedDaysArray = addEmptyCells(firstDayOfWeek).concat(daysArray)
-
-  // Calculate how many empty cells to add at the end
-  const totalCells = 7 * Math.ceil(alignedDaysArray.length / 7)
-  const emptyCellsAtEnd = totalCells - alignedDaysArray.length
-  const finalDaysArray = alignedDaysArray.concat(
-    Array(emptyCellsAtEnd).fill(null),
-  )
-
-  // Split daysArray into chunks of 7 for each week
-  const weeks = chunkArray(finalDaysArray, 7)
+  useEffect(() => {
+    console.log('selectedMonth', selectedMonth)
+    if (
+      habitData &&
+      habitData[habitName!] &&
+      habitData[habitName!][selectedYear] &&
+      habitData[habitName!][selectedYear][selectedMonth]
+    ) {
+      setClickedDays(habitData![habitName!][selectedYear][selectedMonth] || [])
+    } else {
+      setClickedDays([])
+    }
+    const currentDate = new Date()
+    currentDate.setMonth(selectedMonth - 1)
+    const _currentMonthName = currentDate.toLocaleString('default', {
+      month: 'short',
+    })
+    setSelectedMonthName(_currentMonthName)
+    //@ts-ignore
+    setWeeks(getWeeks(selectedMonth, selectedYear))
+  }, [selectedMonth])
 
   // Load saved clickedDays from AsyncStorage
   useEffect(() => {
@@ -66,7 +77,7 @@ export default function ViewHabit() {
         if (savedHabitData !== null) {
           const tmp = JSON.parse(savedHabitData)
           setHabitData(tmp)
-          setClickedDays(tmp[habitName!] || [])
+          setClickedDays(tmp[habitName!][selectedYear][selectedMonth] || [])
         }
       } catch (error) {
         console.error('Error loading habitData from AsyncStorage:', error)
@@ -82,42 +93,59 @@ export default function ViewHabit() {
   useEffect(() => {
     if (loaded) {
       let updatedHabitData = habitData || {}
-      updatedHabitData[habitName!] = clickedDays
+      if (!updatedHabitData[habitName!]) {
+        updatedHabitData[habitName!] = {}
+      }
+
+      if (!updatedHabitData[habitName!][selectedYear]) {
+        updatedHabitData[habitName!][selectedYear] = {}
+      }
+
+      updatedHabitData[habitName!][selectedYear][selectedMonth] = clickedDays
       setHabitData(updatedHabitData)
       localStorage.setItem('habitData', JSON.stringify(updatedHabitData))
     }
   }, [clickedDays, loaded])
 
-  const handlePress = (day: number | null) => {
-    if (day !== null) {
+  const handlePress = (selectedDay: number | null) => {
+    let dt = new Date()
+    let day = dt.getDate()
+    let month = dt.getMonth() + 1
+    let year = dt.getFullYear()
+
+    console.log(day, month, year)
+    console.log(selectedDay, selectedMonth, selectedYear)
+    if (selectedDay !== null) {
       const isFutureDate =
-        year > currentYear ||
-        (year === currentYear && month > currentMonth) ||
-        (year === currentYear && month === currentMonth && day > currentDay)
+        year > selectedYear ||
+        (year === selectedYear && month < selectedMonth) ||
+        (year === selectedYear && month === selectedMonth && day < selectedDay)
+
+      console.log('isFutureDate', isFutureDate)
 
       if (!isFutureDate) {
         setClickedDays((prevClickedDays) =>
-          prevClickedDays.includes(day)
-            ? prevClickedDays.filter((d) => d !== day)
-            : [...prevClickedDays, day],
+          prevClickedDays.includes(selectedDay)
+            ? prevClickedDays.filter((d) => d !== selectedDay)
+            : [...prevClickedDays, selectedDay],
         )
       }
       // Toggle the state for the current day
       if (
-        day === currentDay &&
-        month === currentMonth &&
-        year === currentYear
+        day === selectedDay &&
+        month === selectedMonth &&
+        year === selectedYear
       ) {
         setIsCurrentDayPressed((prev) => !prev)
       }
     }
   }
   return (
-    <div className="flex flex-col p-5 justify-center">
+    <div {...handlers} className="flex flex-col p-5 justify-center">
       <div className="screen">
         <h1 className="text-xl">{habitName?.toUpperCase()}</h1>
       </div>
-      <span className="text-center text-2xl mb-5">{currentMonthName}</span>
+      <span className="text-center text-2xl mb-5">{selectedMonthName}</span>
       <div className="flex justify-evenly items-center pb-1.5">
         <span className="w-8 h-8 flex justify-center items-center text-center">
           S
@@ -141,40 +169,69 @@ export default function ViewHabit() {
           Sa
         </span>
       </div>
-      {weeks.map((week, weekIndex) => (
-        <div
-          key={weekIndex}
-          className="flex justify-evenly items-center pb-1.5"
-        >
-          {week.map((day, dayIndex) => (
+      {
+        //@ts-ignore
+        weeks &&
+          //@ts-ignore
+          weeks!.map((week, weekIndex) => (
             <div
-              key={dayIndex}
-              className="w-8 h-8 flex justify-center items-center text-center relative"
-              onClick={() => handlePress(day)}
+              key={weekIndex}
+              className="flex justify-evenly items-center pb-1.5"
             >
-              <span
-                className={`text-center ${
-                  day === currentDay &&
-                  currentMonth &&
-                  currentYear &&
-                  !isCurrentDayPressed
-                    ? 'border border-blue-500 rounded-full bg-blue-100'
-                    : ''
-                }`}
-              >
-                {day !== null ? day : ''}
-              </span>
-              {clickedDays.includes(day!) && (
-                <div className="absolute top-0 left-0 w-full h-full bg-red-500 bg-opacity-50" />
-              )}
+              {
+                //@ts-ignore
+                week.map((day, dayIndex) => (
+                  <div
+                    key={dayIndex}
+                    className="w-8 h-8 flex justify-center items-center text-center relative"
+                    onClick={() => handlePress(day)}
+                  >
+                    <span
+                      className={`text-center ${
+                        day === selectedDay &&
+                        _currentMonth === selectedMonth &&
+                        _currentYear === selectedYear &&
+                        !isCurrentDayPressed
+                          ? 'border border-blue-500 rounded-full bg-blue-100'
+                          : ''
+                      }`}
+                    >
+                      {day !== null ? day : ''}
+                    </span>
+                    {clickedDays.includes(day!) && (
+                      <div className="absolute top-0 left-0 w-full h-full bg-red-500 bg-opacity-50" />
+                    )}
+                  </div>
+                ))
+              }
             </div>
-          ))}
-        </div>
-      ))}
+          ))
+      }
     </div>
   )
 }
 
+function getWeeks(selectedMonth: number, selectedYear: number) {
+  const daysInMonth = getDaysInMonth(selectedMonth, selectedYear)
+  const firstDayOfWeek = getFirstDayOfWeek(selectedMonth, selectedYear)
+
+  // Generate an array of dates for each day of the month
+  const daysArray = Array.from({ length: daysInMonth }, (_, index) => index + 1)
+
+  // Add empty cells to the beginning to align the start day
+  const alignedDaysArray = addEmptyCells(firstDayOfWeek).concat(daysArray)
+
+  // Calculate how many empty cells to add at the end
+  const totalCells = 7 * Math.ceil(alignedDaysArray.length / 7)
+  const emptyCellsAtEnd = totalCells - alignedDaysArray.length
+  const finalDaysArray = alignedDaysArray.concat(
+    Array(emptyCellsAtEnd).fill(null),
+  )
+
+  // Split daysArray into chunks of 7 for each week
+  const weeks = chunkArray(finalDaysArray, 7)
+  return weeks
+}
 // Function to get the number of days in a month
 function getDaysInMonth(month: number, year: number): number {
   return new Date(year, month, 0).getDate()
